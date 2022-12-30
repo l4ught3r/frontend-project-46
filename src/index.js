bc77fac63ac19bdc2989f2975126dba647b2f7c9
Anton Kuchin <l4ught3r@mail.ru>
2022-12-30 15:10:06 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import format from './formatter/format.js';
 import { getExtension, readFile } from './filePath.js';
 
 const genDiff = (fileName1, fileName2, formatName = 'stylish') => {
-  const obj1 = readFile(fileName1);
-  const obj2 = readFile(fileName2);
-  const extension = getExtension(fileName1);
-  const parseObj1 = parseData(obj1, extension);
-  const parseObj2 = parseData(obj2, extension);
-  const readyDiff = getDiff(parseObj1, parseObj2);
+  const obj1 = parseData(readFile(fileName1), getExtension(fileName1));
+  const obj2 = parseData(readFile(fileName2), getExtension(fileName2));
+  const readyDiff = getDiff(obj1, obj2);
   return format(readyDiff, formatName);
 };
 
